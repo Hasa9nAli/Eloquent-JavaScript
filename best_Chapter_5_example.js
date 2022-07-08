9afe42175c075e18899bb3246680791cec86d28d
Hasan Ali <92353024+Hasa9nAli@users.noreply.github.com>
2022-07-08 20:39:31 +0300

    Update best_Chapter_5_example.js

--- best_Chapter_5_example.js
+++ best_Chapter_5_example.js
@@ -1,4 +1,10 @@
-const repeat(n, action){
+const repeat =(n, action)=>{
   for(let i = 0 ; i < n ; i++)
     action(i)
 }
+repeat(3, console.log)
+let lables = []; 
+repeat(5, i =>{
+lables.push(`unit ${i+1}`)
+})
+console.log(lables) // ['unit 1', 'unit 2','unit 3','unit 4','unit 5' ]
